34e2ff920c386714b41f62fceda990911ffb0f76
wachcio <wachcio@poczta.fm>
2020-10-06 22:59:48 +0200

    Add folders structure. Add React router. Add SCSS

--- log_app/src/index.js
+++ log_app/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './views/Root/index.scss';
+import Root from './views/Root/Root';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
